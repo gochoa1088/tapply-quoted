2cbafb711703609d37cc38c4f858e79920cc0465
Gino Ochoa <gochoa1088@gmail.com>
2023-06-19 19:53:30 -0700

    create email/pass auth functions

--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -1,12 +1,38 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
-import { auth } from "./clientApp";
+import { auth } from "./firebase.config";
 
 export const signInWithGoogle = async () =>
   signInWithPopup(auth, new GoogleAuthProvider());
 
-export const signUpWithEmailAndPassword = async (email, password) => {};
+export const signUpWithEmailAndPassword = async (email, password) => {
+  let result = null;
+  let error = null;
+  try {
+    result = await createUserWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    error = e;
+  }
 
-export const loginWithEmaiAndPassword = async (email, password) => {};
+  return { result, error };
+};
+
+export const loginWithEmaiAndPassword = async (email, password) => {
+  let result = null;
+  let error = null;
+  try {
+    result = await signInWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+};
 
 export const logout = () => signOut(auth);
