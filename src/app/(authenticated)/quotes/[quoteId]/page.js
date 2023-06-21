b22cb09e142bb00ee797aeaae1c30ed577601053
Gino Ochoa <gochoa1088@gmail.com>
2023-06-20 19:30:42 -0700

    delete quote ui

--- src/app/(authenticated)/quotes/[quoteId]/page.js
+++ src/app/(authenticated)/quotes/[quoteId]/page.js
@@ -16,6 +16,16 @@ const Quote = async ({ params: { quoteId } }) => {
         <h1 className="text-red-400 font-semibold pb-2 mb-4 border-b border-borderprimary sm:w-full">
           Delete quote
         </h1>
+        <p className="text-sm">
+          Warning. This action is irreversible. This quote and it's associated
+          data will be deleted.
+        </p>
+        <button
+          className="w-[160px] font-semibold text-sm mt-2 p-2 rounded-md bg-secondary text-red-400
+          border border-slate-400 hover:border-slate-600 hover:bg-highlight"
+        >
+          Delete quote
+        </button>
       </div>
     </div>
   );
